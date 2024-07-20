31e0da6b56b3fc665b8de99803e0b82206bcf683
your_github_username <your_github_email>
2024-07-20 20:05:43 -0300

    feat conection api

--- src/app/shared/components/apartament-table/apartament-table.component.ts
+++ src/app/shared/components/apartament-table/apartament-table.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-apartament-table',
@@ -9,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class ApartamentTableComponent {
 
+  aptos: any[] = [];
+
+  http = inject(HttpClient);
+
+  ngOnInit() {
+    this.http.get<any>('/api/apartamento').subscribe((aptos) => {
+      this.aptos = aptos;
+    });
+  }
 }
